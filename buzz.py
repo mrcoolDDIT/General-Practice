c346f9ac18947570bdd22d4c5f7397a1f71845b3
Ankit <ankitvaghela62@gmail.com>
2018-12-11 21:38:44 +0530

    ..

---
+++ buzz.py
@@ -0,0 +1,11 @@
+limit = int(input("enter limit"))
+for i in range(1,limit+1):
+     if(i%3==0 and i%5==0):
+         print("bizfiz")
+     elif(i%3==0):
+         print("biz")
+     elif(i%5==0):
+         print("fiz")
+
+     else:
+         print(i)
